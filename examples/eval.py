8b6b820795c71d27a2200330fbd74a677b24b7de
njmit123 <nileshnj993@gmail.com>
2021-06-25 22:56:18 +0530

    minor changes

--- examples/eval.py
+++ examples/eval.py
@@ -1,11 +1,4 @@
-a = [5,-5,5,-5,5,5]
-a1=0
-a2=0
-for e in a:
-    if(e%1==0):
-        a1 = a1+e
-        continue
-    if(e%3==0):
-        a2=a2+e
-print(a1,end=" ")
-print(a2)
+def f():
+   '''testing docstrings'''
+
+print(f.__doc__)
